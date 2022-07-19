986a58ca5f5c42fa1bd90e351aac799cce0ca657
DannyRodriguezFSWD <dandemslay@hotmail.com>
2022-07-19 09:56:20 -0600

    FIx some issues
    Fix and the app runs

--- src/dom-manipulation/domManipulation.ts
+++ src/dom-manipulation/domManipulation.ts
@@ -2,16 +2,40 @@ import { DayOfWeek, WeatherIcon, WeatherIcontype, WeatherResponse } from "../mod
 
 // TODO: Create references for all the html elements
 export const buttonClick = document.getElementById("button-location");
+const temperature =  document.getElementById("weather-temp");
+const weatherDescription = document.getElementById("weather-desc");
 const WeatherIconPng = document.getElementById("weather-icon");
-
+const LocationText = document.getElementById("location-text");
+const DateDayName = document.getElementById("date-dayname");
+const DateDay = document.getElementById("date-day");
+const maxTemp = document.getElementById("text-temp-max");
+const minTemp = document.getElementById("text-temp-min");
+const humidity = document.getElementById("text-humidity");
+const wind = document.getElementById("text-wind");
+const locationInput = document.getElementById("weather-location-input");
+export const Spinner = document.getElementById("spinner");
 
 // TODO: Create the logic of the function
 export const updateInteface = (weather: WeatherResponse) :void => {
-    
+    if (temperature) temperature.textContent = Math.floor(weather.main.temp).toString() + "ºC";
+    if (weatherDescription) weatherDescription.textContent = weather.weather[0].main;
+    changeWeatherIcon(weather.weather[0].icon ?? '01d');
+
+    if (LocationText) LocationText.textContent = weather.name;
+    if (DateDayName) DateDayName.textContent = getDayOfWeek();
+    if (DateDay) DateDay.textContent = getDate();
+
+    if (maxTemp) maxTemp.textContent = Math.floor(weather.main.temp_max) + " ºC";
+    if (minTemp) minTemp.textContent = Math.floor(weather.main.temp_min) + " ºC";
+    if (humidity) humidity.textContent = weather.main.humidity.toString() + " %";
+    if (wind) wind.textContent = weather.wind.speed.toString() + " m/s";
 }
 
 // TODO: Get the city from the input element
 export function getCity(): string {
+    if(locationInput) {
+        return (locationInput as HTMLInputElement).value;
+    }
     return "";
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,65 @@
 // Style import
+
+import { getWeather } from './networking/weather';
 import './styles/main.scss';
 
 // Import the API request method
-import { buttonClick, getCity, updateInteface } from './dom-manipulation/domManipulation';
+import { buttonClick, getCity, updateInteface, Spinner } from './dom-manipulation/domManipulation';
+
+export const displayWeather = async () => {
+   //const Spinner = () =>  `<div className="loader"></div>;`
+
+    const city = getCity();
+
+
+    if(city) {
+        const weather = await getWeather(city);
+       
+
+        setTimeout(()=>{ 
+           
+            
+            updateInteface(weather);
+        },600);
+        
+    }
+
+
+
+
+    // try{
+       
+    // } catch(e) {
+    //    // result = e.mesagge;
+    //     if (typeof e === "string"){
+    //         e.toUpperCase() // works, `e` narrowed to string
+    //     } else if (e instanceof Error) {
+    //         e.message // works `e` narrowed to error
+
+    //     } 
+    // } 
+
+
+    /* ()=>{
+    setTimeout(
+        ()=>{ 
+
+        
+        
+    },600);
+    
+}) */
+
+}
+
+if (buttonClick) buttonClick.addEventListener('click', displayWeather);
 
 // Add an event listener to the button
-
+// document.getElementById("button").addEventListener("click",()=>{
+//     setTimeOut(()=>{
+//         document.getElementById("spinner").classList.toggle("invisible")
+//     },600)
+//     dataProcess();
+//     document.getElementById("spinner").classList.toggle("invisible")
+// });
 // Create an async function to call the API method
