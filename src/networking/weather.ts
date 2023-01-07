8d5d6f0563c54492bc05a8a995f3dc5c02eafe74
Jorge <panzera.jorge@gmail.com>
2023-01-07 16:21:04 -0300

    Rama main vacia FUNCIONANDO

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -1,5 +1,7 @@
 import { WeatherResponse } from "../model/weatherResponse";
 
+let city = "Montevideo"
+
 const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`
 
 // TODO: Create an async function with an argument called city to return the that of the endpoint
