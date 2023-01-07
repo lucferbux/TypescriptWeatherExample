ad45ff40a09526744fee6268d9e37730f3ebd8ea
Jorge <panzera.jorge@gmail.com>
2023-01-07 17:24:18 -0300

    Obtener api key

--- src/networking/weather.ts
+++ src/networking/weather.ts
@@ -2,6 +2,14 @@ import { WeatherResponse } from "../model/weatherResponse";
 
 let city = "Montevideo"
 
-const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`
+// Basado en la llamada de https://openweathermap.org/current
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// Api key personal edf15743627917f811f1c682477b6251
+
+//Original
+//const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac6f213887b95d0b8171b342e702e112&units=metric`
+
+// Usando Api Key propia
+const API_CURRENT = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=edf15743627917f811f1c682477b6251&units=metric`
 
 // TODO: Create an async function with an argument called city to return the that of the endpoint
