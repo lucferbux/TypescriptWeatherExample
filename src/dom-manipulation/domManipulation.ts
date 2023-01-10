737e28567ffc6fb358f9e699e81ffe00b1930cac
Jorge Panzera <panzera.jorge@gmail.com>
2023-01-10 12:02:17 -0300

    updateInterface adelanto

--- src/dom-manipulation/domManipulation.ts
+++ src/dom-manipulation/domManipulation.ts
@@ -5,6 +5,10 @@ export const buttonClick = document.getElementById("button-location");
 const WeatherIconPng = document.getElementById("weather-icon");
 const cityImput = document.getElementById("weather-location-input");
 const actual_temperature = document.getElementById("weather-temp");
+const dateDayName = document.getElementById("date-dayname");
+const dateDayNumber = document.getElementById("date-day");
+const locationText = document.getElementById("location-text");
+const weatherIcon = document.getElementById("weather-icon");
 
 
 // TODO: Create the logic of the function
@@ -12,13 +16,32 @@ export const updateInterface = (weather: WeatherResponse) :void => {
     if (actual_temperature !== undefined){
         (actual_temperature as HTMLHeadingElement).textContent= weather.main.temp.toString();
     }
+    if (dateDayName !== undefined) {
+        (dateDayName as HTMLHeadingElement).textContent = getDayOfWeek();
+    }        
+    if (dateDayNumber !== undefined) {
+        (dateDayNumber as HTMLSpanElement).textContent = getDate();
+    }        
+    if (locationText !== undefined) {
+        (locationText as HTMLSpanElement).textContent = weather.name;
+    }            
+
 }
 
 // Crear una funcion para dejar limpio los datos del clima
 export const cleanInterface = (): void => {
     if (actual_temperature !== undefined){
-        (actual_temperature as HTMLHeadingElement).textContent= "";    
+        (actual_temperature as HTMLHeadingElement).textContent= "-";    
     }
+    if (dateDayName !== undefined) {
+        (dateDayName as HTMLHeadingElement).textContent = "-";
+    }        
+    if (dateDayNumber !== undefined) {
+        (dateDayNumber as HTMLSpanElement).textContent = "-";
+    }            
+    if (locationText !== undefined) {
+        (locationText as HTMLSpanElement).textContent = "-";
+    }                
 }
 
 // TODO: Get the city from the input element
