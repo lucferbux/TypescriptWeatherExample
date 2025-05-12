70229e1fd13f8e7f93729e95cd89a0b440d9346f
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2025-05-12 16:31:16 +0200

    feat: Add missing TODOs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-// Style import
 import './styles/main.scss';
 
-// Import the API request method
+// TODO: Import the API request method
 import { buttonClick, getCity, updateInteface } from './dom-manipulation/domManipulation';
 
-// Add an event listener to the button
+// TODO: Add an event listener to the button
 
-// Create an async function to call the API method
+// TODO: Create an async function to call the API method
